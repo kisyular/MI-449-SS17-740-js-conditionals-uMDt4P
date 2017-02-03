5093961095b31ecf129bdd30ad1196579855f179
Rellika Kisyula <kisyular@msu.edu>
2017-02-03 13:22:10 -0500

    Make Changes to the Code to Shorten, through use of string normalization

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ var para = 'After a long night of beautiful dreams, you wake up. You try to adju
 
 var paraAbove18 = 'This game involves strong language (no curse words or offensive words). Do you want to CONTINUE?'
 
-var paraH = 'You finished chopping your left-hand off. A strong wave of unbearable pain hits you and autonomous reflex causes you to throw the saw away. In the room, there are two keys. One is blue and the other is red. CONTINUE?'
+var paraH = 'You finished chopping your left-hand off. A strong wave of unbearable pain hits you and autonomous reflex causes you to throw the saw away. In the room, there are two keys. One is blue and the other is green. CONTINUE?'
 
 var paraL = 'You finished chopping your left-leg off. You start limping across the room. A surge of pain is emanating from the wound. You see two doors, one is Black and the other is White. CONTINUE?'
 
 var paraHB = ' You pick up the blue key. Welcome to Freedom. END.'
 
-var paraHR = 'You pick up the Red key and you start walking through the tiny corridor. You picked the wrong key. You cannot return for the blue one. You have been given another chance, but this time you have to open the door with PIN. Guess the PIN Correctly. CONTINUE?'
+var paraHR = 'You pick up the Green key and you start walking through the tiny corridor. You picked the wrong key. You cannot return for the blue one. You have been given another chance, but this time you have to open the door with PIN. Guess the PIN Correctly. CONTINUE?'
 
 var paraHRC = 'You choose the right PIN. PUSH the door to OPEN. You Eascaped?'
 
@@ -23,24 +23,26 @@ var ans = window.confirm(para)
 if (ans) {
   if (window.confirm(paraAbove18)) {
     var year = window.prompt('To confirm you are older above, enter the YEAR you were born eg (2001, 1983, 1901, etc)')
-    if (year !== null && year !== undefined && year.length === 4) {
+    if (year && year.length === 4) {
       year = parseInt(year)
-      if ((2017 - year) > 18) {
+      if (2017 - year > 18) {
         window.alert('You are Over 18. Click OK to continue')
         var leghand = window.prompt('To cut your leg enter(L) and to cut your hand enter (H)')
-        if (leghand !== null || leghand !== undefined) {
-          leghand = leghand.toLowerCase()
-          leghand = leghand.trim()
+        // if (leghand !== null && leghand !== undefined)
+        if (leghand) {
+          leghand = leghand || ''
+          leghand = leghand.trim().toLowerCase()
           if (leghand === 'h') {
             if (window.confirm(paraH)) {
               var bluered = window.prompt('Enter B to pick the Blue key and G to pick the Green key or R for Computer to Randomly choose a choice for you ')
-              if (bluered !== null || bluered !== undefined) {
-                bluered = bluered.toLowerCase()
-                bluered = bluered.trim()
+              // if (bluered !== null && bluered !== undefined)
+              if (bluered) {
+                bluered = bluered || ''
+                bluered = bluered.trim().toLowerCase()
                 if (bluered === 'r') {
                   var randN = Math.floor((Math.random() * 2) + 1)
                   if (randN === 2) {
-                    bluered = 'r'
+                    bluered = 'g'
                     window.alert('Computer Selected Choice ' + bluered.toUpperCase())
                   } else {
                     bluered = 'b'
@@ -49,10 +51,10 @@ if (ans) {
                 }
                 if (bluered === 'b') {
                   window.alert(paraHB)
-                } else if (bluered === 'r') {
+                } else if (bluered === 'g') {
                   if (window.confirm(paraHR)) {
                     var pin = window.prompt('Enter the pin below. PIN HINT: sort the list (8, 0, 6, 7) ')
-                    if (pin !== null || pin !== undefined) {
+                    if (pin !== null && pin !== undefined) {
                       pin = pin.toLowerCase()
                       pin = pin.trim()
                       if (pin === '0678') {
@@ -78,9 +80,10 @@ if (ans) {
           } else if (leghand === 'l') {
             if (window.confirm(paraL)) {
               var blackwhite = window.prompt('Enter W to open the White door or B to open the Black door ')
-              if (blackwhite !== null && blackwhite !== undefined) {
-                blackwhite = blackwhite.toLowerCase()
-                blackwhite = blackwhite.trim()
+              // if (blackwhite !== null && blackwhite !== undefined)
+              if (blackwhite) {
+                blackwhite = blackwhite || ''
+                blackwhite = blackwhite.trim().toLowerCase()
                 if (blackwhite === 'b') {
                   window.alert(paraLB)
                 } else if (blackwhite === 'w') {
